d2aeae1455b3aa0705c52fdd99f1132ab2c1f79c
Valerii Pozhydaiev <p_vf@i.ua>
2023-06-18 16:22:19 +0300

    add commander

--- contacts.js
+++ contacts.js
@@ -4,39 +4,35 @@ const { nanoid } = require("nanoid/async");
 
 const contactsPath = path.join(__dirname, "db/contacts.json");
 
-const updateContacts = async (contacts) => {
+const update = async (contacts) => {
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 };
 
-// Отримати всі контакти
-const listContacts = async () => {
+const list = async () => {
   const data = await fs.readFile(contactsPath);
   return JSON.parse(data);
 };
 
-// Отримати контакт за id
-const getContactById = async (contactId) => {
-  const contacts = await listContacts();
+const getById = async (contactId) => {
+  const contacts = await list();
   const result = contacts.find((contact) => contact.id === contactId);
   return result || null;
 };
 
-// Видалити контакт за id
-const removeContact = async (contactId) => {
-  const contacts = await listContacts();
+const remove = async (contactId) => {
+  const contacts = await list();
   const contactIndex = contacts.findIndex(
     (el) => el.id === contactId
   );
   if (contactIndex === -1) throw Error("No such contact");
   const removedContact = contacts[contactIndex];
   contacts.splice(contactIndex, 1);
-  await updateContacts(contacts);
+  await update(contacts);
   return removedContact;
 };
 
-// Додати новий контакт
-const addContact = async (name, email, phone) => {
-  const contacts = await listContacts();
+const add = async (name, email, phone) => {
+  const contacts = await list();
   const doubleContact = contacts.find((el) => el.name === name);
   if (doubleContact) throw Error("This contact already exists");
   const newContact = {
@@ -46,13 +42,13 @@ const addContact = async (name, email, phone) => {
     phone,
   };
   contacts.push(newContact);
-  await updateContacts(contacts);
+  await update(contacts);
   return newContact;
 };
 
 module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
+  list,
+  getById,
+  remove,
+  add,
 };
--- index.js
+++ index.js
@@ -1,52 +1,38 @@
 const contacts = require("./contacts");
-const argv = require("yargs").argv;
-const { hideBin } = require("yargs/helpers");
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
-    case "listContacts":
-      const allContacts = await contacts.listContacts();
+    case "list":
+      const allContacts = await contacts.list();
       console.log(allContacts);
       break;
-    case "getContactById":
-      const contact = await contacts.getContactById(id);
+    case "getById":
+      const contact = await contacts.getById(id);
       console.log(contact);
       break;
-    case "removeContact":
-      const removeContact = await contacts.removeContact(id);
+    case "remove":
+      const removeContact = await contacts.remove(id);
       console.log(removeContact);
       break;
-    case "addContact":
-      const addContact = await contacts.addContact(
-        name,
-        email,
-        phone
-      );
+    case "add":
+      const addContact = await contacts.add(name, email, phone);
       console.log(addContact);
       break;
     default:
-      break;
+      console.warn("\x1B[31m Unknown action type!");
   }
 };
 
-const arr = hideBin(process.argv);
-// console.log(argv);
-
 invokeAction(argv);
-// invokeAction({
-//   action: "listContacts",
-// });
-// invokeAction({
-//   action: "getContactById",
-//   id: "vza2RIzNGIwutCVCs4mCL",
-// });
-// invokeAction({
-//   action: "addContact",
-//   name: "Mango",
-//   email: "mango@gmail.com",
-//   phone: "322-22-22",
-// });
-// invokeAction({
-//   action: "removeContact",
-//   id: "vQIPOQcHo0xous1bLLrAf",
-// });
